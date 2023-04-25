59719890c3b689aa495dcc5d192552be35fa2cb4
IrinaLes1 <irina.les@utair.ru>
2023-04-25 20:42:44 +0300

    feat: added create post page pet-13

---
+++ pages/createPost.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+/** Components */
+import Topic from '../shared/ui/components/Topic';
+
+/** Layouts */
+import MainContainer from '../shared/ui/layouts/MainLayout';
+import Box from '../shared/ui/layouts/Box';
+
+/** Models */
+import Post from '../entities/Post/model/Post';
+
+/** Interfaces */
+import IPost from '../entities/Post/model';
+
+const CreatePost = () => {
+    const [post, setPost] = useState(new Post());
+
+    const onChangeInput = (type: string) => (e) => {
+        switch (type) {
+            default: {
+                setPost((prevPost: IPost) => ({
+                    ...prevPost,
+                    [type]: e.target.value,
+                }));
+            }
+        }
+    };
+
+    const onSubmit = () => {};
+
+    return (
+        <MainContainer keywords="posts" title="Main">
+            <Box flexWrap="no-wrap">
+                <Box gap={64} width="100%" px={64} my={64}>
+                    <Topic text="Create Post" />
+                    <form onSubmit={onSubmit}>
+                        <Box flexDirection="column" gap={24}>
+                            <input type="text" value={post.title} onInput={onChangeInput('title')} />
+                            <input type="text" value={post.body} onInput={onChangeInput('body')} />
+                            <input type="submit" value="Publish" />
+                        </Box>
+                    </form>
+                </Box>
+            </Box>
+        </MainContainer>
+    );
+};
+
+export default CreatePost;
